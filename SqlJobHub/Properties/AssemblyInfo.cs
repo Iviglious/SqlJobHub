c84c8bac7007b7fbe6f253944ff7ff4ef6dc1e8a
Iviglious <ivo_shalev@yahoo.com>
2017-11-23 17:55:55 +0000

    Amended to desired settings.

--- SqlJobHub/Properties/AssemblyInfo.cs
+++ SqlJobHub/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SqlJobHub")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# app for handling and monitoring SQL jobs.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SqlJobHub")]
+[assembly: AssemblyCompany("Iviglious")]
+[assembly: AssemblyProduct("SQL Job Hub")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
